22d094add42025f280b84174de3e064cb5c39573
carlosefernandess <cfernandescso@gmail.com>
2022-10-20 14:52:27 -0300

    Resolve o exercício do tópico Tipos Primitivos...

---
+++ fundamentos/secao-4-introducao-a-javascript/dia-1-javascript-primeiros-passos/scripts2.js
@@ -0,0 +1,6 @@
+const base = 5;
+const height = 8;
+const area = base * height;
+console.log("A área do retângulo é: " + area);
+const perimeter = (2*base) + (2*height);
+console.log("O perímetro do retângulo é: " + perimeter)
