e6f673f6f0ee85038ea69adf174f17dbd7c89842
carlosefernandess <cfernandescso@gmail.com>
2022-10-20 15:11:04 -0300

    Resolve o exercício do tópico Condições if e Else

---
+++ fundamentos/secao-4-introducao-a-javascript/dia-1-javascript-primeiros-passos/scripts3.js
@@ -0,0 +1,13 @@
+const scoreStudent = 99.93;
+
+if (scoreStudent >= 80) {
+    console.log("Parabéns, você foi aprovado(a)!");
+}
+
+else if (scoreStudent < 80 && scoreStudent > 60) {
+    console.log("Você está na nossa lista de espera");
+}
+
+else {
+    console.log ("Você foi reprovado(a)");
+}
