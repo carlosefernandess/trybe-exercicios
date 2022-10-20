5072734f4bdd9b84ef93c61923dd1d4319a0fa6d
carlosefernandess <cfernandescso@gmail.com>
2022-10-20 16:17:19 -0300

    Resolve a primeira parte dos exercícios, do tópico Exerxícios

---
+++ fundamentos/secao-4-introducao-a-javascript/dia-1-javascript-primeiros-passos/exercicio-scripts1.js
@@ -0,0 +1,21 @@
+// Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b, definidas no começo com os valores que serão operados. Faça programas para:
+// Adição (a + b)
+// Subtração (a - b)
+// Multiplicação (a * b)
+// Divisão (a / b)
+// Módulo (a % b)
+
+const a = 13.13
+const b = 66.6
+
+let adicao = (a+b);
+let subtracao = (a-b);
+let multiplicacao = (a*b);
+let divisao = (a/b);
+let modulo = (a%b);
+
+console.log("O resultado da adição é:" + " " + adicao);
+console.log("O resultado da substração é:" + " " + subtracao);
+console.log("O resultado da multiplicação é:" + " " + multiplicacao);
+console.log("O resultado da divisão é:" + " " + divisao);
+console.log("O resto da divisão entre " + a + " e " + b + ", ou seja, o módulo, é: " + modulo);
