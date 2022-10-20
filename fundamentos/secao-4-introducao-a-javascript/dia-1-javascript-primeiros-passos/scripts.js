2c011d693a5308b29469948ff65484c202746b0d
carlosefernandess <cfernandescso@gmail.com>
2022-10-20 14:29:15 -0300

    Resolve o exercício do tópico Variáveis

---
+++ fundamentos/secao-4-introducao-a-javascript/dia-1-javascript-primeiros-passos/scripts.js
@@ -0,0 +1,8 @@
+const myNAme = "Carlos";
+const birthCity = "Sertãozinho/SP";
+let birthYear = "1992";
+console.log(myNAme, birthCity, birthYear);
+birthYear = "2030";
+console.log(birthYear)
+birthCity = "Gotham City"
+// O código apresenta erro por tentar reatribuir à CONSTANTE "birthCity" novo valor; somente "let e var" podem ser alterados.
