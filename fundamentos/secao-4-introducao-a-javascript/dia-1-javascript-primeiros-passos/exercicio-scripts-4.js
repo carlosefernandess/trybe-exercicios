67237a3596a12a45b4c873b75fb2247fd1c67e3b
carlosefernandess <cfernandescso@gmail.com>
2022-10-20 20:13:12 -0300

    Resolve o exercício com referido título no comentário do arquivo

---
+++ fundamentos/secao-4-introducao-a-javascript/dia-1-javascript-primeiros-passos/exercicio-scripts-4.js
@@ -0,0 +1,13 @@
+// Utilize if/else para fazer um programa que, dado um valor recebido como parâmetro, retorne “positive” se esse valor for positivo, “negative” se for negativo, e caso não seja nem positivo e nem negativo retorne “zero”.
+
+let numberInsert = 56
+
+if (numberInsert > 0) {
+    console.log("positive")
+}
+else if (numberInsert < 0) {
+    console.log("negative")
+}
+else {
+    console.log("zero")
+}
