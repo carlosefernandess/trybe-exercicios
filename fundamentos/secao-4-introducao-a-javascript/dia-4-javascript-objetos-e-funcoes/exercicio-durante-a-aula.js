e8dd74bf56c39c59e924470fd0c17e4f3fee0e9a
carlosefernandess <cfernandescso@gmail.com>
2022-10-25 19:16:35 -0300

    Resolve o exercício do tópico Objetos

---
+++ fundamentos/secao-4-introducao-a-javascript/dia-4-javascript-objetos-e-funcoes/exercicio-durante-a-aula.js
@@ -0,0 +1,14 @@
+// Crie uma variável player e atribua um objeto contendo as variáveis listadas abaixo
+
+// let name = 'Marta';
+// let lastName = 'Silva';
+// let age = 34;
+// let medals = { golden: 2, silver: 3 };
+
+let player = {
+nome : 'Marta',
+lastName :'Silva',
+age : 34,
+medals : { golden: 2, silver: 3 },};
+// MEDALS É UM OBJETO DENTRO DE UM OBJETO
+console.log(player['medals']['silver'])
