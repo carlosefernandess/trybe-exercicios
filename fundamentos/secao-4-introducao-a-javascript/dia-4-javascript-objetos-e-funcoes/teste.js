6e403871b3656921bf56e0ebbee4af26bd5bbe42
carlosefernandess <cfernandescso@gmail.com>
2022-10-27 14:33:45 -0300

    Salva os arquivos antes de começar o projeto...27-10

---
+++ fundamentos/secao-4-introducao-a-javascript/dia-4-javascript-objetos-e-funcoes/teste.js
@@ -0,0 +1,17 @@
+// SOMA TODOS OS NÚMEROS DE 0 A 100 (1+2+3+4...+100)
+
+// let resultado = 0;
+
+// for (index=0;index<=100;index+=1) {
+// resultado += index;
+// }
+// console.log(resultado);
+
+// QUANTAS VEZES PODE SUBSTRAIR 5 DE 100
+
+let resultadoSubtrai = 0;
+
+for (index=100;index>=0;index-5) {
+    resultadoSubtrai += index;
+};
+console.log (resultadoSubtrai)
