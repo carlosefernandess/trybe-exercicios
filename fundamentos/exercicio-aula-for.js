36298ae4a98390ddea14e557c347b976c7bdf9f2
carlosefernandess <cfernandescso@gmail.com>
2022-11-08 21:11:00 -0300

    Resolve exercícios da seção 6, dia 1 - seletores DOM

--- fundamentos/exercicio-aula-for.js
+++
@@ -1,5 +0,0 @@
-// 1)
-
-let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
-for (let groceryIndex=0; groceryIndex<groceryList.length; groceryIndex +=1)
-console.log(groceryList[groceryIndex]);
