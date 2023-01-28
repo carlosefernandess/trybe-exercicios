692e6ce79669dafb2563505f642b70be1bb2f2e0
carlosefernandess <cfernandescso@gmail.com>
2023-01-27 22:26:44 -0300

    Realiza os exercícios propostos durante a aula

---
+++ front-end/secao-2-introducao-ao-react/dia-01-hello-world-no-react/my-app/src/About.js
@@ -0,0 +1,21 @@
+import React, {Component} from "react";
+
+class About extends Component {
+    render () {
+        return <div>
+            <h1>Carlos Eduardo Fernandes</h1>
+            <p>Pessoa estudante de Desenvolvimento Web na Trybe 🖥️ </p>
+            <h2>Minhas habilidades: </h2>
+            <ul>
+                <li>Escuta ativa</li>
+                <li>Comunicação</li>
+                <li>JavaScript</li>
+                <li>Jest</li>
+                <li>React</li>
+            </ul>
+            </div>
+
+    }
+}
+
+export default About;
---
+++ front-end/secao-2-introducao-ao-react/dia-01-hello-world-no-react/nome-da-aplicacao/src/HelloWorld.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class HelloWorld extends Component {
+    render () {
+        return <h2 className='HelloWorld'>Hello, <big>world</big>!</h2>;
+    };
+};
+
+export default HelloWorld;
