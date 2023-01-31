631360393157e0ab8c85e44aeaf3ff7149998c51
carlosefernandess <cfernandescso@gmail.com>
2023-01-31 00:49:27 -0300

    Realiza o exercício fancy-buttons, refatorando conforme orientado, de modo que as funções criadas agora estão dentro da classe e são chamadas através do this.função. Ainda mais, o this original das funções foi vinculado ao this da classe através do método bind(this), que corrige uma 'falha' do JS, que não foi criado para trabalhar com classes. Assim, é possível acesar as props e usá-las dentro das funções criadas por mim.

--- front-end/secao-3-componentes-com-estado-eventos-e-formularios-react/fancy-buttons/src/App.js
+++ front-end/secao-3-componentes-com-estado-eventos-e-formularios-react/fancy-buttons/src/App.js
@@ -1,24 +1,38 @@
 import React, {Component} from  'react';
 import './App.css';
 
-function consoleText1 () {
-  return console.log(`Clicou no botão 1! :D`)
-};
-function consoleText2 () {
-  return console.log(`Clicou no botão 2! :D`)
-};
-function consoleText3 () {
-  return console.log(`Clicou no botão 3! :D`)
-}
+
+
+
 
 
 class App extends Component {
+  constructor () {
+    super ()
+    this.consoleText1 = this.consoleText1.bind(this);
+    this.consoleText2 = this.consoleText2.bind(this);
+    this.consoleText3 = this.consoleText3.bind(this);
+  }
+
+  consoleText1() {
+  console.log('Clicou no botão 1! :D')
+  };
+
+  consoleText2 () {
+  console.log(`Clicou no botão 2! :D`)
+  };
+
+  consoleText3 () {
+  console.log(`Clicou no botão 3! :D`)
+  };
+
   render () {
-  return (<div>
-    <button onClick={consoleText1}>Imprime Texto 1 no Console</button>
-    <button onClick={consoleText2}>Imprime Texto 2 no Console</button>
-    <button onClick={consoleText3}>Imprime Texto 3 no Console</button>
-        </div>
+  return (
+    <div>
+      <button onClick={this.consoleText1}>Imprime Texto 1 no Console</button>
+      <button onClick={this.consoleText2}>Imprime Texto 2 no Console</button>
+      <button onClick={this.consoleText3}>Imprime Texto 3 no Console</button>
+    </div>
   );
   };
 }
